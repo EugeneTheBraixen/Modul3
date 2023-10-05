f58a7806dfb4569afc9b62a86cf9e32e2eb4c226
EugeneTheBraixen <100828745+EugeneTheBraixen@users.noreply.github.com>
2023-10-05 21:21:56 +0300

    Add files via upload

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+delegate void TaskDelegate(string task);
+
+class TaskManager
+{
+    private List<string> tasks = new List<string>();
+    private List<TaskDelegate> delegates = new List<TaskDelegate>();
+
+    //добавление задачи
+    public void AddTask(string task)
+    {
+        tasks.Add(task);
+    }
+
+    //добавление делегата для выполнения задачи
+    public void AddTaskDelegate(TaskDelegate taskDelegate)
+    { 
+        delegates.Add(taskDelegate);
+    }
+
+    //выполнение задачи с выбранным делегатом
+    public void ExecuteTasks()
+    {
+        for (int i = 0; i < tasks.Count; i++) 
+        { 
+            if (i < delegates.Count)
+            {
+                delegates[i](tasks[i]);
+            }
+            else
+            {
+                Console.WriteLine($"Задача \"{tasks[i]}\" не имеет делегата для выполнения.");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TaskManager taskManager = new TaskManager();
+
+        //добавление задач
+        taskManager.AddTask("Задача 1");
+        taskManager.AddTask("Задача 2");
+        taskManager.AddTask("Задача 3");
+
+        //добавление делегатов для выполнения задач
+        taskManager.AddTaskDelegate((task) => Console.WriteLine($"Выполнена задача: {task} (Отправка уведомления..)"));
+        taskManager.AddTaskDelegate((task) => Console.WriteLine($"Выполнена задача: {task} (Запись в журнал..)"));
+
+        //выполнение задач
+        taskManager.ExecuteTasks();
+
+        Console.ReadLine();
+    }
+}
---
+++ Zadanie5/Zadanie5/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+delegate void SortDelegate(List<int> numbers);
+
+class SortingApp
+{
+    public static void BubbleSort(List<int> numbers)
+    {
+        bool swapped;
+        do
+        {
+            swapped = false;
+            for (int i = 0; i < numbers.Count; i++) 
+            {
+                if (numbers[i] > numbers[i + 1])
+                {
+                    int temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
+
+    public static void QuickSort(List<int> numbers, int left, int right)
+    {
+        if (left < right)
+        {
+            int pivotIndex = Partition(numbers, left, right);
+            QuickSort(numbers, left, pivotIndex - 1);
+            QuickSort(numbers, pivotIndex + 1, right);
+        }
+    }
+
+    private static int Partition(List<int> numbers, int left, int right)
+    {
+        int pivot = numbers[right];
+        int i = left - 1;
+
+        for (int j = right - 1; j < right; j++)
+        {
+            if (numbers[j] < pivot)
+            {
+                i++;
+                int temp = numbers[j];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+
+        int temp2 = numbers[i + 1];
+        numbers[i + 1] = numbers[right];
+        numbers[right] = temp2;
+
+        return i + 1;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int> {5, 2, 9, 1, 5, 6, 3};
+
+        Console.WriteLine("Выберите метод сортировки:");
+        Console.WriteLine("1. Сортировка пузырьком");
+        Console.WriteLine("2. Быстрая сортировка");
+
+        int choice = int.Parse(Console.ReadLine());
+        SortDelegate sortDelegate = null;
+
+        switch(choice) 
+        {
+            case 1:
+                sortDelegate = SortingApp.BubbleSort; 
+                break;
+
+            case 2:
+                sortDelegate = (list) => SortingApp.QuickSort(list, 0, list.Count - 1); 
+                break;
+
+            default:
+                Console.WriteLine("Неверный выбор");
+                break;
+        }
+
+        if (sortDelegate != null)
+        {
+            sortDelegate(numbers);
+            Console.WriteLine("Отсортированные числа: ");
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
+}
